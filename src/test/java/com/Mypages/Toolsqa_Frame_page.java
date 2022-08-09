c0e0f2c31b3c3cfce7c267bb4a227e3b7b41188a
agarwalshrutika <ajayi@LAPTOP-2547OPBP>
2022-08-08 20:51:54 -0700

    Page class for toolsQA frame validation

--- src/test/java/com/Mypages/Toolsqa_Frame_page.java
+++ src/test/java/com/Mypages/Toolsqa_Frame_page.java
@@ -2,14 +2,10 @@ package com.Mypages;
 
 import org.openqa.selenium.By;
 
-public class Frame_page {
+public class Toolsqa_Frame_page {
 	
 	public static By firstFrame_demoqa = By.xpath("//*[@id=\"frame1\"]");
 	public static By TextFromFrame = By.xpath("//*[@id=\"sampleHeading\"]");
 	public static By SecondFrame_demoqa = By.xpath("//*[@id=\"frame2\"]");
-	
-	public static By nested_outerFrame = By.id("frame1");
-	public static By nested_Topic_box = By.tagName("input");
-	
 	 
 }
