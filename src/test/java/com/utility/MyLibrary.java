2d7d2a3ff45d8ce78a7c477f7f0845bbb45d96e2
agarwalshrutika <ajayi@LAPTOP-2547OPBP>
2022-08-10 20:11:13 -0700

    test commit

--- src/test/java/com/utility/MyLibrary.java
+++ src/test/java/com/utility/MyLibrary.java
@@ -11,7 +11,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.asserts.Assertion;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -19,16 +22,16 @@ public class MyLibrary {
 	public static Properties objPro;
 	public static WebDriver driver;
 
-	public void ReadProperties() throws IOException {
+	public static void ReadProperties() throws IOException {
 		System.out.println("file path is :" + System.getProperty("user.dir"));
 		File objFile = new File(System.getProperty("user.dir") + "//src//test//resources//configuration.properties");
 		FileInputStream ObjIS = new FileInputStream(objFile);
 		objPro = new Properties();
 		objPro.load(ObjIS);
-		
+
 	}
 
-	public void LaunchBrowserandGmoOnline() {
+	public static void LaunchBrowserandGmoOnline() {
 
 		String browserfrompropertiesfile = objPro.getProperty("browser");
 		switch (browserfrompropertiesfile) {
@@ -50,11 +53,14 @@ public class MyLibrary {
 			driver = new EdgeDriver();
 			break;
 		}
-		driver.get(objPro.getProperty("GmoOnlineUrl"));
+
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-		
+}
 
-	}
-
+	 public static void ExplicitWaitForAlert() {
+		 WebDriverWait wait = new WebDriverWait(driver, ConstantValues.ExplicitWait);
+		//  WebDriverWait wait = new WebDriverWait(driver,ConstantValues.ExplicitWait);
+		  wait.until(ExpectedConditions.alertIsPresent());
+	 }
 }
