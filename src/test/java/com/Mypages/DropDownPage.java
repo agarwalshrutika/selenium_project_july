18ecf81548366e2f86d4a3821f410d518412392a
agarwalshrutika <ajayi@LAPTOP-2547OPBP>
2022-08-14 17:59:45 -0700

    DropDown Assignment on Facebook URL

---
+++ src/test/java/com/Mypages/DropDownPage.java
@@ -0,0 +1,10 @@
+package com.Mypages;
+
+import org.openqa.selenium.By;
+
+public class DropDownPage {
+	public static By birth_month = By.xpath("//select[@name='birthday_month']");
+	public static By birth_day = By.xpath("//select[@name='birthday_day']");
+	public static By birth_year = By.xpath("//select[@name='birthday_year']");
+
+}
---
+++ src/test/java/com/MytestNg/ValidateDragAndDrop.java
@@ -0,0 +1,9 @@
+package com.MytestNg;
+
+import org.testng.annotations.Test;
+
+public class ValidateDragAndDrop {
+  @Test
+  public void f() {
+  }
+}
